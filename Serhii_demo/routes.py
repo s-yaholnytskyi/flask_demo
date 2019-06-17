4bfe5011e94100aa55eea21050e518aaf96706e9
s-yaholnytskyi <s.yaholnytskyi@gmail.com>
2019-06-17 13:17:24 +0300

    added about page

--- Serhii_demo/routes.py
+++ Serhii_demo/routes.py
@@ -16,6 +16,11 @@ def home():
     return render_template('home.html')
 
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
 @app.route('/upload')
 def upload():
     return render_template('upload.html')
